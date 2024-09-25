015622e1e5e3606e333e6619e6bff6ce3d1fc49d
Mennaeshra12 <menna12mohamed.20@gmail.com>
2024-09-25 17:05:37 +0200

    watchlist 

--- lib/homeScreen/homeScreenLogic/HomeScreenViewModel .dart
+++ lib/homeScreen/homeScreenLogic/HomeScreenViewModel .dart
@@ -1,10 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movies_watchlist/WatchList/watchListTab.dart';
 
 import '../../BrowserScreen/Widgets/browserTabScreen.dart';
 import '../../HomeTab/widgets/homeTab.dart';
 import '../../SerchScreen/Widgets/searchSceenTab.dart';
-// import '../../WatchList/Widgets/watchListTab.dart';
 import 'homeScreenStates.dart';
 
 class HomeScreenViewModel extends Cubit<HomeScreenStates> {
@@ -16,7 +16,7 @@ class HomeScreenViewModel extends Cubit<HomeScreenStates> {
     Hometab(),
     Searchsceentab(),
     BrowserTabScreen(),
-    // WatchListWidget()
+    WatchListTab()
   ];
   void channgeSelecedIndex(int newIndex) {
     emit(HomeInitialstate());
